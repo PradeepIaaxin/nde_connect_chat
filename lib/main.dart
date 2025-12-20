693051b7f2561bf922a14ff577fbb777499ed200
PradeepIaaxin <pradeep@iaaxin.com>
2025-12-20 10:21:20 +0530

    UI updates

--- lib/main.dart
+++ lib/main.dart
@@ -184,25 +184,3 @@ Future<void> initializeStorage() async {
   ]);
   await FlutterDownloader.initialize(debug: false);
 }
-
-
-
-
-//correction 
-//1. IN chatlisrt - > header. - done 
-//2. in chat ui fornt , time , secnorio and image , ui 
-// 3. in chat list > font bold ness and time formate and colour  - done 
-//4. bottom icon 
-//5,app logout transion  
-//6 , nnetowrk secnior 
-//7, in grop chat name displaying different colours  
-//8, search bar implemenation in header  - done 
-//9, header icons follow same formate   
-// while texting new message automaticallyb we need to disokay upper case  - done 
-// 10 . in card all cards ui abd and card ui // 
-//11 ,typing suguesstion 
-//12 , in side chat screen we need to remove shadow and border from appbar
-//13,  
-
-// retry onoine and update connection ..
-//
