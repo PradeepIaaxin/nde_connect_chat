63c8308dfaa5301631dd8e419519e2c769d046aa
Napradeep <pradeepdeva7432@gmail.com>
2026-01-24 17:03:07 +0530

    notification reply and ui updated

--- lib/utils/fcm_handler/fcm_handler.dart
+++ lib/utils/fcm_handler/fcm_handler.dart
@@ -1,21 +1,10 @@
-// import 'package:firebase_messaging/firebase_messaging.dart';
-// import 'local_notification_service.dart';
-
-// Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-//   print("🔥 BACKGROUND FCM: ${message.data}");
-//   await LocalNotificationService.initialize();
-//   await LocalNotificationService.show(message);
-// }
-
-
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:nde_email/utils/services/whatsapp_notification_engine.dart';
+import 'package:nde_email/utils/fcm_handler/local_notification_service.dart';
 
 @pragma('vm:entry-point')
 Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  print("🔥 BACKGROUND FCM: ${message.data}");
-  
-  await WhatsAppNotificationEngine.init();
-  await WhatsAppNotificationEngine.show(message);
+  await Firebase.initializeApp();
+  await LocalNotificationService.initialize();
+  await LocalNotificationService.show(message);
 }
