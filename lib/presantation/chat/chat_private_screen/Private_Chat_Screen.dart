82522c0514e903e1c4c995441319746bbd8d99b2
AravindAravind12345 <144437758+AravindAravind12345@users.noreply.github.com>
2026-01-30 10:26:21 +0530

    errors fixed

--- lib/presantation/chat/chat_private_screen/Private_Chat_Screen.dart
+++ lib/presantation/chat/chat_private_screen/Private_Chat_Screen.dart
@@ -5092,8 +5092,15 @@ class _PrivateChatScreenState extends State<PrivateChatScreen> {
                                                             MixedMediaViewer(
                                                           items: conversationMedia,
 
-                                                          initialIndex: startIndex < 0 ? 0 : startIndex,
-                                                          currentUserId: currentUser,
+                                                          initialIndex:
+                                                          startIndex < 0
+                                                              ? 0
+                                                              : startIndex,
+                                                      currentUserId:
+                                                          currentUser,
+                                                      isGroup: false,
+                                                      receiverId:
+                                                          widget.receiverId,
 
 
 
